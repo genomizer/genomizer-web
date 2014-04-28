1d02e76079692ea356271a8305f4e05f868629ae
Linda Sandstrom <oi11lsm@cs.umu.se>
2014-04-28 16:52:50 +0200

    Updated search, search template

--- app/js/views/search/Search.js
+++ app/js/views/search/Search.js
@@ -1,11 +1,34 @@
-define(['collections/SearchResults'],function(SearchResults) {
+define([
+	'collections/SearchResults',
+	'text!views/search/searchInputGroup.html'
+],function(SearchResults, inputGroupTemplate) {
+
 	var Search = Backbone.View.extend({
+
+		TEMPLATE: _.template(inputGroupTemplate),
 		initialize: function(query) {
 			this.model = new SearchResults({query:query});
 			this.render();
 		},
+		el: $("#search"),
 		render: function() {
-			this.$el.html("Search!");
+			this.$el.html(this.TEMPLATE());	
+		},
+		events: {
+			"click #search_button": "doSearch",
+			"keyup #search_input": "showButton"
+		},
+		showButton: function() {
+			if($('#search_input').val().length != 0) {
+				$('#search_button').prop('disabled', false);
+				console.log("showbutton");
+			} else {
+				$('#search_button').prop('disabled', true);
+			}
+
+		},
+		doSearch: function() {
+			alert("Searching for "+$('#search_input').val() +'.');
 		}
 		
 	});
