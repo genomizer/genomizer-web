f9a55fca8ac6c79ac9d22c59a8a695a6173b4345
Viktor Barsk <c11vbk@cs.umu.se>
2014-04-28 16:45:48 +0200

    SearchResults collection

---
+++ test/js/tests/collections/SearchResults.js
@@ -0,0 +1,27 @@
+define(['collections/SearchResults'],function(SearchResults) {
+
+	describe("Testing SearchResults", function () {
+			var requests;
+			beforeEach(function () {
+    				xhr = sinon.useFakeXMLHttpRequest();
+    				requests = [];
+				xhr.onCreate = function (req) { requests.push(req); };
+			});
+			afterEach(function () {
+				// Like before we must clean up when tampering with globals.
+				xhr.restore();
+			});
+			describe("should fetch with correct URL when query is supplied as option",function() {
+				it("regular string", function () {
+					var searchResults = new SearchResults([],{query:"male[sex]"});
+					searchResults.fetch();
+					expect(requests[0].url).to.equal('/api/search?q=male[sex]');
+				});
+				it("string with spaces", function () {
+					var searchResults = new SearchResults([],{query:"male[sex] AND specie[cat]"});
+					searchResults.fetch();
+					expect(requests[0].url).to.equal('/api/search?q=male[sex] AND specie[cat]');
+				});
+			});
+	});
+});
