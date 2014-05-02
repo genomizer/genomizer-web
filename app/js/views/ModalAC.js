731e46f8f9d306dfc0c421a1d61350dc72b2de52
mickelus <wikner@cs.umu.se>
2014-05-02 12:06:21 +0200

    made modal always navigate back to search view, change to use router previous method

--- app/js/views/ModalAC.js
+++ app/js/views/ModalAC.js
@@ -10,8 +10,15 @@ define([
 			this.$modal = $(this.MODAL_TEMPLATE(this.TEMPLATE_VARS));
 			this.$el = this.$modal.find(".modal-inner-content");
 
+			
+
 		},
 		show: function () {
+
+			// store previous url
+			console.log("router", Backbone.history.fragment)
+
+			// render
 			this.render();
 			this.$modal.appendTo($("body"));
 			this.$modal.modal('show');
@@ -19,8 +26,11 @@ define([
 		},
 		hide: function () {
 			this.$modal.modal('hide');
+			
 		},
 		removeModal: function() {
+
+			app.router.navigate('search', {trigger:false});
 			this.$modal.remove();
 
 		}
