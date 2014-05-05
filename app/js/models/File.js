4cd1b338dd0f4dcba72f00384bed0cfc8533379a
Viktor Barsk <c11vbk@cs.umu.se>
2014-05-05 13:19:50 +0200

    File upload done notification

--- app/js/models/File.js
+++ app/js/models/File.js
@@ -28,21 +28,31 @@ define([],function() {
 				username: "pvt",
 				password: "pvt",
 				processData: false,
-				contentType: false /*,
+				contentType: false,
+		/*		beforeSend: function(jqXHR) {
+					debugger;
+					jqXHR.upload.addEventListener("progress",_.bind(that.setUploadProgress,that), false);
+				}
 				xhr: function()
 				{
 					//Upload progress
-					var xhr = $.ajaxSettings.xhr() ;
+					var xhr = $.ajaxSettings.xhr();
 					xhr.upload.addEventListener("progress",_.bind(that.setUploadProgress,that), false);
 					return xhr;
-				} */
-			});
+				} 
+*/
+			}).done(_.bind(this.setUploadDone,this));
 		},
 		setUploadProgress:function(evt) {
 			if (evt.lengthComputable) {
 				this.progress = evt.loaded / evt.total;
 				this.trigger("uploadProgress",this.progress);
 			}
+		},
+		setUploadDone: function() {
+			this.uploadDone = true;
+			this.progress = 1;
+			this.trigger("uploadProgress");
 		}
 	});
 	return File;
--- app/js/views/upload/FileUploadView.js
+++ app/js/views/upload/FileUploadView.js
@@ -5,7 +5,7 @@ function(File) {
 	var FileUploadView = Backbone.View.extend({
 		TEMPLATE: _.template('<%- fileName %> \
 							 <div class="progress"></div>'),
-		PROGRESS_TEMPLATE:_.template('<div class="progress-bar" role="progressbar" aria-valuenow="<%- progress %>" aria-valuemin="0" aria-valuemax="100" style="width: <%- progress %>%;"></div>'),
+		PROGRESS_TEMPLATE:_.template('<div class="progress-bar <%- done ? "progress-bar-success" : "" %>" role="progressbar" aria-valuenow="<%- progress %>" aria-valuemin="0" aria-valuemax="100" style="width: <%- progress %>%;"></div>'),
 		initialize: function() {
 			this.model.on("uploadProgress",this.renderProgress,this);
 		},
@@ -16,7 +16,10 @@ function(File) {
 			this.renderProgress();
 		},
 		renderProgress: function() {
-			this.$(".progress").html(this.PROGRESS_TEMPLATE({progress: Math.round(this.model.progress*100)}));
+			this.$(".progress").html(this.PROGRESS_TEMPLATE({
+				progress: Math.round(this.model.progress*100),
+				done:this.model.uploadDone
+			}));
 		}
 	});
 	return FileUploadView;
