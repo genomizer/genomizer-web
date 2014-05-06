8725b749c06314161adf5e7bb4471c2ec3c8b1cc
gatsuu <joelpersso@gmail.com>
2014-05-06 17:08:55 +0200

    Refactored Search.js

--- app/js/views/search/Search.js
+++ app/js/views/search/Search.js
@@ -16,20 +16,20 @@ define([
 				collection: this.collection,
 				annotations: app.annotationTypes
 			});
-      this.collection.on("highlightChange", this.showDownloadAndProcessButtons, this);
+
+			this.collection.on("highlightChange", this.showDownloadAndProcessButtons, this);
 			this.render();
 		},
 		el: $("#search"),
 		render: function() {
-      console.log('current query: ', this.collection.query)
 			this.$el.html(this.TEMPLATE());
-			this.$el.find('#search_input').val(this.collection.query);
 
+			this.$el.find('#search_input').val(this.collection.query);
 			this.$el.find('#results_container').append(this.resultsView.$el);
 
-      if(this.collection.query != undefined) {
-        this.showSearchButton();
-      }
+			if(this.collection.query != undefined) {
+				this.showSearchButton();
+			}
 		},
 		events: {
 			"submit #search_form": "doSearch",
@@ -37,18 +37,21 @@ define([
 			"click #download_button": "downloadSelected",
 			"click #process_button": "processSelected"
 		},
-    showDownloadAndProcessButtons: function(fileArray) {
-      if(fileArray.length != 0) {
-        $('#download_button').prop('disabled', false);
-      } else {
-        $('#download_button').prop('disabled', true);
-      }
-      if(fileArray.length == 1 && fileArray[0].get("type") == "raw") {
-        $('#process_button').prop('disabled', false);
-      } else {
-        $('#process_button').prop('disabled', true);
-      }
-    },
+		showDownloadAndProcessButtons: function(fileArray) {
+			//handles whether or not the download button should be clickable.
+			if(fileArray.length != 0) {
+				$('#download_button').prop('disabled', false);
+			} else {
+				$('#download_button').prop('disabled', true);
+			}
+
+			//handles whether or not the process button should be clickable.
+			if(fileArray.length == 1 && fileArray[0].get("type") == "raw") {
+				$('#process_button').prop('disabled', false);
+			} else {
+				$('#process_button').prop('disabled', true);
+			}
+		},
 		showSearchButton: function() {
 			if($('#search_input').val().length != 0) {
 				$('#search_button').prop('disabled', false);
@@ -58,31 +61,30 @@ define([
 
 		},
 		doSearch: function() {
-			console.log("In doSearch: Searching for "+$('#search_input').val() +'.');
-			//searchResults skickar AJAX REQUEST och uppdaterar innehållet i dess container.
-      app.router.navigate('search/' + $('#search_input').val(), {trigger:true});
+			//navigates to the given searchquery and searches for the given query.
+			app.router.navigate('search/' + $('#search_input').val(), {trigger:true});
 			this.collection.setSearchQuery($('#search_input').val());
 		},
 		downloadSelected: function() {
-			//Create hidden downloader
+			//Create hidden downloader to download the url given as parameter.
 			var downloadURL = function downloadURL(url) {
-			    var hiddenIFrameID = 'hiddenDownloader',
+					var hiddenIFrameID = 'hiddenDownloader',
 				iframe = document.getElementById(hiddenIFrameID);
-			    if (iframe === null) {
+					if (iframe === null) {
 				iframe = document.createElement('iframe');
 				iframe.id = hiddenIFrameID;
 				iframe.style.display = 'none';
 				document.body.appendChild(iframe);
-			    }
-			    iframe.src = url;
-
+					}
+					iframe.src = url;
 			};
 
-      var URLsToDownload = this.collection.getSelectedFileURLs();
-      for (var i = 0; i < URLsToDownload.length; i++) {
-        alert('Starting to download from url: '+URLsToDownload[i]);
-        downloadURL(URLsToDownload[i]);
-      };
+			//Downloads all the selected files.
+			var URLsToDownload = this.collection.getSelectedFileURLs();
+			for (var i = 0; i < URLsToDownload.length; i++) {
+				alert('Starting to download from url: '+URLsToDownload[i]);
+				downloadURL(URLsToDownload[i]);
+			};
 
 		},
 		processSelected: function() {
@@ -96,5 +98,3 @@ define([
 	});
 	return Search;
 });
-
-
