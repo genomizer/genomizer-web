4ced760771b6c2a8486ba3b66d2cded813f625d0
Viktor Barsk <c11vbk@cs.umu.se>
2014-05-07 08:26:59 +0200

    Clean up of tests (minor styling)

--- test/js/tests/models/Experiment.js
+++ test/js/tests/models/Experiment.js
@@ -1,22 +1,7 @@
 define(['models/Experiment'],function(Experiment) {
+	describe("models/Experiment", function () {
+		it("TODO",function() {
 
-	describe("Testing Experiment model", function () {
-			it("should have default name'", function () {
-				var experiment = new Experiment();
-				expect(experiment.get("name")).to.equal("Experiment1");
-			});
-			it("should have hatt 3 '", function () {
-				var experiment = new Experiment();
-				expect(experiment.hatt).to.equal(3);
-			});
-			it("should have female '", function () {
-				var experiment = new Experiment();
-				expect(experiment.get("sex")).to.equal("Female");
-			});
-			it("should have species: cat '", function () {
-				var experiment = new Experiment();
-				expect(experiment.get("species")).to.equal("Cat");
-			});
-
+		});
 	});
 });
