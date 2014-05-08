a5b7502969098409cb7347e8a46c5cc7e5a13e6b
Magnus <dv12msn@cs.umu.se>
2014-05-08 08:19:40 +0200

    Testing solutions to edit-view

--- app/js/router.js
+++ app/js/router.js
@@ -1,96 +1,107 @@
 define([],function() {
-	var Router = Backbone.Router.extend({
+    var Router = Backbone.Router.extend({
 
-		routes: {
-			"search/:query": "search",
-			"search": "search",
-			"upload":"upload",
-			"process":"process",
-			"admin":"admin", 
-			"admin/createannotation": "createAnnotation",
-			"admin/editannotation": "editAnnotation"
-		},
+        routes: {
+            "search/:query": "search",
+            "search": "search",
+            "": "search",
+            "upload":"upload",
+            "process":"process",
+            "admin":"admin",
+            "admin/createannotation": "createAnnotation",
+            "admin/editannotation/:id": "editAnnotation"
+        },
 
-		initialize: function(options) {
-			this.history = [];
-			this.on("all", this.storeRoute);
-		},
+        initialize: function(options) {
+            this.history = [];
+            this.on("route", this.storeRoute);
+        },
 
-		storeRoute: function() {
-			this.history.push(Backbone.history.fragment);
-		},
+        storeRoute: function(event, p2) {
+            console.log("Router > storeRoute > event: ", event, p2);
+            this.history.push(Backbone.history.fragment);
+        },
 
-		previous: function(options) {
-			if (this.history.length > 1) {
-				if(options) {
-					this.navigate(this.history.pop(), options);
-				} else {
-					this.navigate(this.history.pop());
-				}
-				
-			}
-		},
+        previous: function(options) {
+            console.log("router > previous > history: ", this.history);
+            if (this.history.length > 2) {
 
-		search: function(query) {
-			var that = this;
-			require([
-				'views/search/Search'
-			],function(Search) {
-				new Search({el:that.getNewMainView(),query:query});
-			});
-		},
+                // remove the current page from history
+                this.history.pop();
 
-		upload: function() {
-			var that = this;
-			require([
-				'views/upload/Upload'
-			],function(Upload) {
-				new Upload({el:that.getNewMainView()});
-			});
-		},
+                // get the previous page
+                var previous = this.history.pop();
 
-		process: function() {
-			require([
-				'views/processModal/Process'
-			],function(Process) {
-				var modal = new Process();
-				modal.show();
-			});
-		},
-		getNewMainView: function() {
-			$("#mainView").replaceWith('<section id=mainView></section>');
-			return $("#mainView");
-		},
-		
-		admin: function() {
-			var that = this;
-			require([
-				'views/sysadmin/SysadminMainView',
-				'views/sysadmin/AnnotationsView'
-			],function(SysadminMainView, AnnotationsView) {
-				new SysadminMainView({el:that.getNewMainView()});
-				new AnnotationsView({el:that.getNewMainView()});
-			});
-		},
-		
-		createAnnotation: function() {
-			var that = this;
-			require([
-				'views/sysadmin/NewAnnotationView'
-			],function(NewAnnotationView) {
-				new NewAnnotationView({el:that.getNewMainView()});
-			});
-		},
-		
-		editAnnotation:	function() {
-			var that = this;
-			require([
-				'views/sysadmin/EditAnnotationView'
-			],function(EditAnnotationView) {
-				new EditAnnotationView({el:that.getNewMainView()});
-			});
-		}	
+                console.log("router > previous > previouspath: ", previous);
+                this.navigate(previous, options);
+            }
+        },
+        hasPrevious: function() {
+            return this.history.length > 2;
+        },
 
-	});
-	return Router;
+        search: function(query) {
+            var that = this;
+            require([
+                'views/search/Search'
+            ],function(Search) {
+                new Search({el:that.getNewMainView(),query:query});
+            });
+        },
+
+        upload: function() {
+            var that = this;
+            require([
+                'views/upload/Upload'
+            ],function(Upload) {
+                new Upload({el:that.getNewMainView()});
+            });
+        },
+
+        process: function() {
+            require([
+                'views/processModal/Process'
+            ],function(Process) {
+                var modal = new Process();
+                modal.show();
+            });
+        },
+        getNewMainView: function() {
+            $("#mainView").replaceWith('<section id=mainView></section>');
+            return $("#mainView");
+        },
+        
+        admin: function() {
+            var that = this;
+            require([
+                'views/sysadmin/SysadminMainView',
+                'views/sysadmin/AnnotationsView'
+            ],function(SysadminMainView, AnnotationsView) {
+                new SysadminMainView({el:that.getNewMainView()});
+                new AnnotationsView({el:that.getNewMainView()});
+            });
+        },
+        
+        createAnnotation: function() {
+            var that = this;
+            require([
+                'views/sysadmin/NewAnnotationView'
+            ],function(NewAnnotationView) {
+                new NewAnnotationView({el:that.getNewMainView()});
+            });
+        },
+        
+        editAnnotation:    function(id) {
+            var that = this;
+            require([
+                'views/sysadmin/EditAnnotationView'
+            ],function(EditAnnotationView) {
+                new EditAnnotationView({el:that.getNewMainView(), id:id});                
+            });
+        }    
+
+    });
+    return Router;
 });
+
+
--- app/js/views/sysadmin/EditAnnotationView.js
+++ app/js/views/sysadmin/EditAnnotationView.js
@@ -1,78 +1,93 @@
 define(['text!templates/sysadmin/EditTemplate.html', 'models/sysadmin/Annotation', 'collections/sysadmin/Annotations', 'models/sysadmin/Gateway'], function(editTemplate, annotation, annotations, Gateway) {
 
-	var EditView = Backbone.View.extend({
-		events : {
-			"click #updateAnnotation" : "updateAnnotation",
-			"click #deleteAnnotation" : "deleteAnnotation"
-		},
+    var EditView = Backbone.View.extend({
+        events : {
+            "click #updateAnnotation" : "updateAnnotation",
+            "click #deleteAnnotation" : "deleteAnnotation"
+        },
 
-		updateAnnotation : function() {
-			if ($('#edit_annotation_name').val() === "") {
-				alert("Annotation name is required");
-				return;
-			}
-			
-			this.annotation.set({
-				"name" : $('#edit_annotation_name').val()
-			});
+        updateAnnotation : function() {
+            if ($('#edit_annotation_name').val() === "") {
+                alert("Annotation name is required");
+                return;
+            }
+            
+            this.annotation.set({
+                "name" : $('#edit_annotation_name').val()
+            });
 
-			this.annotation.set({
-				"forced" : $('#edit_annotation_forced').val()
-			});
+            this.annotation.set({
+                "forced" : $('#edit_annotation_forced').val()
+            });
 
-			var temp = $('#edit_annotation_value').val();
+            var temp = $('#edit_annotation_value').val();
 
-			temp = temp.split(",");
-			this.annotation.set({
-				"type" : temp
-			});
+            temp = temp.split(",");
+            this.annotation.set({
+                "type" : temp
+            });
 
-			this.annotation.set({
-				"default" : temp[0]
-			});
+            this.annotation.set({
+                "default" : temp[0]
+            });
 
-			alert("Annotation saved!");
-		},
+            alert("Annotation saved!");
+        },
 
-		deleteAnnotation : function() {
-			var x = window.confirm("Are you sure you want to remove this annotation?");
-			if (x) {
-				var y = window.confirm("Annotation will be completely removed!");
-				if (y) {
-					var payload = new Backbone.Model();
-					payload.set({
-						'delete' : [this.annotation.get('name')]
-					});
+        deleteAnnotation : function() {
+            var x = window.confirm("Are you sure you want to remove this annotation?");
+            if (x) {
+                var y = window.confirm("Annotation will be completely removed!");
+                if (y) {
+                    var payload = new Backbone.Model();
+                    payload.set({
+                        'delete' : [this.annotation.get('name')]
+                    });
 
-					var result = Gateway.deleteAnnotation(payload);
+                    var result = Gateway.deleteAnnotation(payload);
 
-					alert("Annotation removed!");
-				}
-			}
-		},
+                    alert("Annotation removed!");
+                }
+            }
+        },
 
-		setLocalAnnotation : function(annotation, annotations) {
-			this.annotation = annotation;
-			this.annotations = annotations;
-		},
+        setLocalAnnotation : function(annotation, annotations) {
+            this.annotation = annotation;
+            this.annotations = annotations;
+        },
 
-		render : function(annotation, annotations) {
-			var template = _.template(editTemplate, {
-				annotation : annotation,
-				annotations : annotations
-			});
+        render : function(annotation, annotations) {
 
-			this.$el.html(template);
-			
-			if (annotation.get('value') == 'freetext') {
-				$('#edit_annotation_value').attr('disabled', '');
-			} else {
-				$('#edit_annotation_value').removeAttr('disabled');
-			}
-		},
-		initialize : function() {
-			this.render();
-		}
-	});
-	return EditView;
+            var template = _.template(editTemplate, {
+                annotation : annotation,
+                annotations : annotations
+            });
+
+            this.$el.html(template);
+            if (annotation.get('value') == 'freetext') {
+                $('#edit_annotation_value').attr('disabled', '');
+            } else {
+                $('#edit_annotation_value').removeAttr('disabled');
+            }
+        },
+        initialize : function() {
+            
+            var that = this;
+            annotations = new annotations();    
+            annotations.fetch({
+                success : function(annotations) {
+                    annotation = annotations.getAnnotationByID(that.id);
+                    that.annotation = annotation;
+                    that.annotations = annotations;
+                    console.log(that.id);
+                    // that.setLocalAnnotation(annotation, annotations);
+                }
+            });
+            console.log(that.annotation);
+            this.render(this.annotation, this.annotations);
+        }
+    });
+    return EditView;
 });
+
+
