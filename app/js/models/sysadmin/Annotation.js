a89d57e13f04eff95015295afcf43822d6e0f897
Love <love.engman@hotmail.com>
2014-05-13 14:54:20 +0200

    tests for filterCollection

--- app/js/models/sysadmin/Annotation.js
+++ app/js/models/sysadmin/Annotation.js
@@ -1,7 +1,6 @@
 define([], function() {
 	var Annotation = Backbone.Model.extend({
 		defaults : {
-			"id" : "0",
 			"name" : "Not specified",
 			"values" : "Not specified",
 			"forced" : "false"
--- test/js/tests/models/Annotation.js
+++ test/js/tests/models/Annotation.js
@@ -6,19 +6,16 @@ define(['models/sysadmin/Annotation'],function(Annotation) {
 			});
 			it("Should have correct default values", function () {
 				var annotation = new Annotation();
-				annotation.get('id').should.equal('0');
 				annotation.get('name').should.equal('Not specified');
 				annotation.get('values').should.equal('Not specified');
 				annotation.get('forced').should.equal('false');
 			});
 				it("Should have correct values after setting them", function () {
 				var annotation = new Annotation();
-				annotation.set({'id':'1'});
 				annotation.set({'name':'Ape'});
 				annotation.set({'values':'Monkey'});
 				annotation.set({'forced':'true'});
 				
-				annotation.get('id').should.equal('1');
 				annotation.get('name').should.equal('Ape');
 				annotation.get('values').should.equal('Monkey');
 				annotation.get('forced').should.equal('true');
