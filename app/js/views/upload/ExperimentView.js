e3e518f646bb1da0d72ed14da12116f89b80229f
Viktor Barsk <c11vbk@cs.umu.se>
2014-05-19 14:51:29 +0200

    Success color when file upload are done

--- app/js/views/upload/ExperimentView.js
+++ app/js/views/upload/ExperimentView.js
@@ -40,8 +40,12 @@ function(ExperimentTemplate,AnnotationsForm,FileUploadList,Experiment) {
 			this.fileUploadList.render();
 		},
 		renderUploadProgress: function() {
-			var progress = this.model.files.getTotalUploadProgress() * 100;
-			this.$('.panel-heading').css('background','linear-gradient(to right, rgb(91, 192, 222) 0%, rgb(91, 192, 222) '+ progress +'%,#f5f5f5 ' + (Math.min(100,progress + 0.0001)) + '%, #f5f5f5)');
+			if(!this.model.files.hasUnfinishedUploads() && this.model.files.length) {
+				this.$('.panel-heading').css('background','#5cb85c');
+			} else {
+				var progress = this.model.files.getTotalUploadProgress() * 100;
+				this.$('.panel-heading').css('background','linear-gradient(to right, #428bca 0%, #428bca '+ progress +'%,#f5f5f5 ' + (Math.min(100,progress + 0.0001)) + '%, #f5f5f5)');
+			}
 		},
 		changeLabelName: function() {
  			if(this.model.get('name').length >0) {
