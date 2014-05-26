3c63fb3c1f72660e2e461a4c73cb0bbbb0164770
Magnus <dv12msn@cs.umu.se>
2014-05-26 14:01:02 +0200

    old test removed

--- test/js/tests/collections/GenomeReleaseFiles.js
+++ test/js/tests/collections/GenomeReleaseFiles.js
@@ -70,39 +70,6 @@ define(['collections/sysadmin/GenomeReleaseFiles', 'models/sysadmin/GenomeReleas
 				genomeReleaseFiles.models[0].get('genomeVersion').should.equal('aa53');
 			});
 		});
-		describe("Tests of orderBy(fileName)", function (){
-				it("Should sort genomeReleaseFiles correctly when using orderBy(fileName)", function() {
-				var GenomeReleaseFile1 = new GenomeReleaseFile({
-					"genomeVersion" : "hg1337",
-					"specie" : "owl",
-					"path" : "/somewhere/on/drive/hg1337/",
-					"fileName" : "abc123.fasta"
-				});
-				var GenomeReleaseFile2 = new GenomeReleaseFile({
-					"genomeVersion" : "mg17",
-					"specie" : "rat",
-					"path" : "/somewhere/on/drive/mg17/",
-					"fileName" : "zzz.fasta"
-				});
-				var GenomeReleaseFile3 = new GenomeReleaseFile({
-					"genomeVersion" : "hu66",
-					"specie" : "baby rabbit",
-					"path" : "/somewhere/on/drive/hu66/",
-					"fileName" : "filename.fasta"
-				});
-				var GenomeReleaseFile4 = new GenomeReleaseFile({
-					"genomeVersion" : "aa53",
-					"specie" : "human",
-					"path" : "/somewhere/on/drive/aa53/",
-					"fileName" : "curb.fasta"
-				});
-
-				var genomeReleaseFiles = new GenomeReleaseFiles([GenomeReleaseFile1, GenomeReleaseFile2, GenomeReleaseFile3, GenomeReleaseFile4]);
-				
-				genomeReleaseFiles.orderBy("fileName");
-				genomeReleaseFiles.models[0].get('genomeVersion').should.equal('hg1337');
-			});
-		});
 		describe("Tests of getGenomeReleaseByName", function (){
 				it("Should retrieve correct file names when using getGenomeReleaseByName", function() {
 				var genomeRelease1 = new GenomeReleaseFile({
