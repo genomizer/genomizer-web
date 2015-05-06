27f7c10e3c46cdf117c381d4bc42fd1d34f3329e
niklasf <niklasf@cs.umu.se>
2015-05-06 22:14:57 +0200

    now only 2 files for test, but "view" still required

--- sandbox/mochatest/test/js/tests.js
+++ sandbox/mochatest/test/js/tests.js
@@ -5,22 +5,5 @@
 	To add new tests just add the test file and its path to this script.
 */
 var testModulePaths = [
-	// Collections
-	//'tests/collections/SearchResults',
-	//'tests/collections/Files',
-	//'tests/collections/Annotations',
-	//'tests/collections/AnnotationTypes',
-	//'tests/collections/GenomeReleaseFiles',
-	//'tests/collections/Experiments',
-    //
-	//// Models
-	//'tests/models/Experiment',
-	//'tests/models/File',
-	//'tests/models/Annotation',
-	//'tests/models/Gateway',
-	//'tests/models/GenomeReleaseFile',
-
-	//// Views
-	//'tests/views/ModalAC',
-	'tests/views/MainMenu',
+	'tests/MainMenuTest',
 ];
---
+++ sandbox/mochatest/test/js/tests/MainMenuTest.js
@@ -0,0 +1,6 @@
+define(['MainMenu'],function(MainMenu) {
+	describe("views/MainMenuTest", function () {
+		it("should register render on route change", function () {
+		});
+	});
+});
