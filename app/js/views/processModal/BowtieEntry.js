405f4c84cc404e6a81e3ace9f2d1875e1ce0fe5f
niklasf <niklasf@cs.umu.se>
2015-05-18 20:04:20 +0200

    bowtie input gets stored in model

--- app/js/views/processModal/BowtieEntry.js
+++ app/js/views/processModal/BowtieEntry.js
@@ -1,15 +1,36 @@
 define([
-    'text!templates/processModal/BowtieEntry.html'
-], function(bowtieEntryTemplate) {
+    'text!templates/processModal/BowtieEntry.html',
+    'models/File'
+], function(bowtieEntryTemplate, File) {
     return Backbone.View.extend({
         TEMPLATE: _.template(bowtieEntryTemplate),
 
         initialize: function(options) {
+            this.model = new File();
+            this.model.clear();
+            this.updateModel();
         },
         events: {
+            "change input": "updateModel",
+            //"click #close": "updateModel"
         },
         render: function() {
             this.$el.html(this.TEMPLATE());
+        },
+        updateModel: function() {
+            var input = {};
+            console.log(this.$("input"));
+            this.$("input").each(function() {
+                var $this = $(this);
+                var val = $this.val();
+                var placeHolder = $this.attr("placeholder");
+                if (val.length == 0 && placeHolder !== undefined) {
+                    val = placeHolder;
+                }
+                input[$this.attr("name")] = val;
+            });
+            this.model.set(input);
+            console.log(this.model);
         }
     });
 });
