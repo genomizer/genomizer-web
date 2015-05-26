926ffa3591d188277acea9458d7c0148520ca7cc
Anna Jonsson <aj@cs.umu.se>
2015-05-26 13:44:37 +0200

    Fixed enabling/disabling in searcbh view

--- app/js/views/search/Search.js
+++ app/js/views/search/Search.js
@@ -31,7 +31,7 @@ define([
 				collection: this.collection,
 				annotations: app.annotationTypes.withoutExpID()
 			});
-			this.collection.on("highlightChange", this.showButtons, this);
+			this.collection.on("highlightChange", this.enableDisableButtons, this);
 
 			this.render();
 
@@ -133,43 +133,49 @@ define([
 			}
 			deleteExperiments();
 		},
-		showButtons: function(fileArray) {
+		enableDisableButtons: function(fileArray) {
 
 			var selectedFiles = this.collection.getSelectedFiles();
 			var selectedExperiments = this.collection.getSelectedExperiments();
+
 			//handles whether or not the upload or process buttons should be clickable.
+			$('#process_button').addClass('disabled');
+			$('#upload_button').addClass('disabled');
+
 			if(selectedExperiments.length > 0) {
 				$('#upload_button').removeClass('disabled');
-				$('#process_button').removeClass('disabled');
 
 				//Makes sure there is two raw files in selected experiments and all have same species.
-				var startSpecie = this.collection.getSpeciesForExperiment(selectedExperiments.at(0).get("name"));
-				for(var i = 0; i < selectedExperiments.length; i++) {
-					var specie = this.collection.getSpeciesForExperiment(selectedExperiments.at(i).get("name"));
-					if(startSpecie != specie) {
-						$('#process_button').addClass('disabled');
-						break;
-					}
-					var expFiles = selectedExperiments.at(i).get("files");
-					if(expFiles.length == 0) {
-						$('#process_button').addClass('disabled');
-						break;
-					} else {
-						var nrOfRawFiles = 0;
-						for(var j = 0; j < expFiles.length;j++) {
-							if(expFiles[j].type.toLowerCase() == "raw") {
-								nrOfRawFiles++;
-							}
-						}
-						if(nrOfRawFiles!=2) {
-							$('#process_button').addClass('disabled');
-							break;
-						}
-					}
-				}
-			} else {
-				$('#upload_button').addClass('disabled');
-				$('#process_button').addClass('disabled');
+				// var startSpecie = this.collection.getSpeciesForExperiment(selectedExperiments.at(0).get("name"));
+				
+				// for(var i = 0; i < selectedExperiments.length; i++) {
+				// 	var specie = this.collection.getSpeciesForExperiment(selectedExperiments.at(i).get("name"));
+				// 	if(startSpecie != specie) {
+				// 		$('#process_button').addClass('disabled');
+				// 		break;
+				// 	}
+				// 	var expFiles = selectedExperiments.at(i).get("files");
+				// 	if(expFiles.length == 0) {
+				// 		$('#process_button').addClass('disabled');
+				// 		break;
+				// 	} else {
+				// 		var nrOfRawFiles = 0;
+				// 		for(var j = 0; j < expFiles.length;j++) {
+				// 			if(expFiles[j].type.toLowerCase() == "raw") {
+				// 				nrOfRawFiles++;
+				// 			}
+				// 		}
+				// 		if(nrOfRawFiles!=2) {
+				// 			$('#process_button').addClass('disabled');
+				// 			break;
+				// 		}
+				// 	}
+				// }
+
+			}
+
+			if (selectedExperiments.length == 1) {
+				$('#process_button').removeClass('disabled');
 			}
 
 			//handles whether or not the download or delete buttons should be clickable.
