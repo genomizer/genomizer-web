e477103533d2c627b25fbe83c033ea854fa89f7c
Pascal Hansson <pascal@motionshuset.se>
2015-05-27 16:40:09 +0200

    Fixed delete process status

--- app/js/collections/ProcessStatuses.js
+++ app/js/collections/ProcessStatuses.js
@@ -1,7 +1,7 @@
 define(['models/ProcessStatus'],function(ProcessStatus) {
 	var ProcessStatuses = Backbone.Collection.extend({
 		url: function() {
-			return app.BASE_URL + 'process/';
+			return app.BASE_URL + 'process/dummy';
 		},
 		model: ProcessStatus,
 		initialize:function (options) {
