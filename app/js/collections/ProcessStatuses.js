d4b78897fc6f8833682ad67dfd74a229dea3df0a
Pascal Hansson <pascal@motionshuset.se>
2015-05-27 16:45:03 +0200

    Forgot /...

--- app/js/collections/ProcessStatuses.js
+++ app/js/collections/ProcessStatuses.js
@@ -1,7 +1,7 @@
 define(['models/ProcessStatus'],function(ProcessStatus) {
 	var ProcessStatuses = Backbone.Collection.extend({
 		url: function() {
-			return app.BASE_URL + 'process';
+			return app.BASE_URL + 'process/';
 		},
 		model: ProcessStatus,
 		initialize:function (options) {
