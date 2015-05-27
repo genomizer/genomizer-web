2303d0349517e7f841e44ab742c03ff23476367a
Pascal Hansson <c12phn@cs.umu.se>
2015-05-27 11:16:37 +0200

    Fixed the process status view

--- app/js/views/MainMenu.js
+++ app/js/views/MainMenu.js
@@ -7,26 +7,26 @@ define([
 		initialize: function(options) {
 			_.bindAll(this,'render')
 			options.router.on("route",this.render);
-			// this.processPopover = new ProcessPopover({ collection: app.processStatuses });
-			// this.processPopover.on("show", this.onProcessShow, this);
-			// this.processPopover.on("hide", this.onProcessHide, this);
+			this.processPopover = new ProcessPopover({ collection: app.processStatuses });
+			this.processPopover.on("show", this.onProcessShow, this);
+			this.processPopover.on("hide", this.onProcessHide, this);
 		},
 		render: function(route) {
 			this.$el.html(this.TEMPLATE({route:route}));
-			// this.$el.find("#process_container").append(this.processPopover.$el);
+			this.$el.find("#process_container").append(this.processPopover.$el);
 		},
-		// events: {
-		// 	"click #process_container > a" : "openProcessPopover"
-		// },
-		// openProcessPopover: function() {
-		// 	this.processPopover.show();
-		// },
-		// onProcessShow: function() {
-		// 	this.$el.find("#process_container").addClass("open");
-		// },
-		// onProcessHide: function() {
-		// 	this.$el.find("#process_container").removeClass("open");
-		// }
+		events: {
+			"click #process_container > a" : "openProcessPopover"
+		},
+		openProcessPopover: function() {
+			this.processPopover.show();
+		},
+		onProcessShow: function() {
+			this.$el.find("#process_container").addClass("open");
+		},
+		onProcessHide: function() {
+			this.$el.find("#process_container").removeClass("open");
+		}
 		
 	});
 	return MainMenu;
